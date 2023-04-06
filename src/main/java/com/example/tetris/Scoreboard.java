7a49d556c3437ae54212e7618e37ece74b16ee11
Nora Langli <norawl@stud.ntnu.no>
2023-04-06 22:48:44 +0200

    addScore adder score!!1!

--- src/main/java/com/example/tetris/Scoreboard.java
+++ src/main/java/com/example/tetris/Scoreboard.java
@@ -1,15 +1,30 @@
 package com.example.tetris;
 
 import javafx.util.Pair;
-
-import java.util.ArrayList;
+import java.util.*;
 
 public class Scoreboard {
     private ArrayList<Pair<String, Integer>> scores = new ArrayList<>();
+    private static final int MAX_SCORES = 10;
 
     public void addScore(String name, int score) {
-        // TODO: Add score in the correct place, and remove the lowest score if there are more than 10
-        scores.add(new Pair<>(name, score));
+
+        // Adds the new score to the scores list
+        Pair<String, Integer> newScore = new Pair<String, Integer>(name, score);
+        scores.add(newScore);
+
+        // Sorts the list in descending order
+        Collections.sort(scores, new Comparator<Pair<String, Integer>>() {
+            @Override
+            public int compare(Pair<String, Integer> o1, Pair<String, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+
+        // If there are more than 10(MAX_SCORE) elements we remove the excess ones.
+        if (scores.size() > MAX_SCORES) {
+            scores.subList(MAX_SCORES, scores.size()).clear();
+        }
     }
 
     public Pair<String, Integer> getScore(int index) {
