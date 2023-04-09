173525f95f83929705bfd4959c378d1be54d63ce
Nora Langli <norawl@stud.ntnu.no>
2023-04-09 15:58:11 +0200

    mappestruktur for test

---
+++ src/test/java/com/example/tetris/ScoreboardTest.java
@@ -0,0 +1,13 @@
+package com.example.tetris;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScoreboardTest {
+
+    // Teste edge cases (score skrevet som string feks, kanskje name er fakka?)
+    // Teste at flere navn kan lagres flere ganger
+    // Teste at scores sorteres i riktig rekkefølge
+    // Teste at antall scores ikke overskrider max
+    // Teste fillagring
+}
---
+++ src/test/java/com/example/tetris/TetrisTest.java
@@ -0,0 +1,10 @@
+package com.example.tetris;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TetrisTest {
+
+    // Test poengkalkulering
+    // Test.... fjerning av rader idk?
+}
